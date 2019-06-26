1e9d35d8438517705d73c95ac61649c1eb4e92c4
Alexandre MEUR <alex.meur@gmail.com>
2019-06-26 12:15:54 +0200

    Router avec un menu

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,15 +1,15 @@
-import {Routes} from '@angular/router';
-import {PATH_HOME, PATH_DETAIL} from './app.routes.constantes';
+import {Routes} from "@angular/router";
+import {PATH_DETAIL, PATH_HOME} from "./app.routes.constantes";
 import {HomeComponent} from "./home/home.component";
 import {DetailComponent} from "./detail/detail.component";
 
 export const ROUTES: Routes = [
-    {
-      path: PATH_HOME,
-      component: HomeComponent,
-    },
-    {
-      path: PATH_DETAIL,
-      component: DetailComponent,
-    },
-  ];
+  {
+    path: PATH_HOME,
+    component: HomeComponent,
+  },
+  {
+    path: PATH_DETAIL,
+    component: DetailComponent,
+  },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
+import {AppRoutingModule} from "./app-routing.module";
 import {RouterModule} from "@angular/router";
 import {ROUTES} from "./app-routes";
 
