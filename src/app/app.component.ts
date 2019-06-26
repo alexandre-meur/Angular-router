4c93ed409cc0522fa8570e10d6fd3573f1e8f16e
Alexandre MEUR <alex.meur@gmail.com>
2019-06-26 15:40:34 +0200

    Event du router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Routeur';
   idDetail = 1;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(
+          e => e instanceof NavigationStart
+            || e instanceof NavigationEnd
+            || e instanceof NavigationError
+            || e instanceof NavigationCancel
+        )
+      )
+      .subscribe(e => {
+        console.log('Event : ', e.constructor.name, e);
+      });
+  }
 }
